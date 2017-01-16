153aab4d436ae35e6111034074b17808c53f0eb7
davidqi <david.qi@otms.com>
2017-01-16 19:51:44 +0800

    a few modifications

---
+++ controller/src/main/java/com/startter/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.startter.controller;
+
+import com.startter.common.IController;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by davidqi on 1/16/17.
+ */
+@RestController
+public class UserController implements IController {
+
+}
