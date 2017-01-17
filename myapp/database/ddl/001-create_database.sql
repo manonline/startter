ca0a3108ead6cb01ffcffd32af7a2b5c05a9980b
davidqi <david.qi@otms.com>
2017-01-17 22:59:36 +0800

    sql scripts

---
+++ myapp/database/ddl/001-create_database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE starter
+  WITH OWNER = starter
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
--- myapp/database/ddl/user.sql
+++ myapp/database/ddl/user.sql
@@ -1,15 +1,11 @@
 CREATE TABLE public."user"
 (
-  id bigint NOT NULL, -- primary key of user table
+  id bigint NOT NULL,
   name text NOT NULL,
-  age integer NOT NULL,
-  gender integer NOT NULL,
-  email text,
-  phone text,
   CONSTRAINT user_pkey PRIMARY KEY (id)
 )
 WITH (
-  OIDS=FALSE
+OIDS=FALSE
 );
 ALTER TABLE public."user"
-  OWNER TO postgres;
+  OWNER TO starter;
