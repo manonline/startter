de0f626b896004a43427cea1da9852420122ca64
davidqi <david.qi@otms.com>
2017-01-17 17:41:00 +0800

    change module directory

---
+++ myapp/src/main/java/com/manonline/myapp/common/IRepository.java
@@ -0,0 +1,7 @@
+package com.manonline.myapp.common;
+
+/**
+ * Created by davidqi on 1/16/17.
+ */
+public interface IRepository {
+}
---
+++ myapp/src/main/java/com/manonline/myapp/common/IService.java
@@ -0,0 +1,8 @@
+package com.manonline.myapp.common;
+
+/**
+ * Created by davidqi on 1/16/17.
+ */
+public interface IService {
+
+}
