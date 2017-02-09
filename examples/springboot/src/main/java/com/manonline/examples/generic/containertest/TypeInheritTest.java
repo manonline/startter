c97e8a8f2b4f86636b48f554af731ecc6ad969c3
davidqi <david.qi@otms.com>
2017-02-09 12:04:02 +0800

    a few modifications

--- examples/springboot/src/main/java/com/manonline/examples/generic/containertest/TypeInheritTest.java
+++ examples/springboot/src/main/java/com/manonline/examples/generic/containertest/TypeInheritTest.java
@@ -28,7 +28,7 @@ public class TypeInheritTest {
          * 通过getData()方法取出数据时到底是什么类型呢？Integer? Float? 还是Number？且由于在编程过程中的顺序不可控性，
          * 导致在必要的时候必须要进行类型判断，且进行强制类型转换。显然，这与泛型的理念矛盾
          */
-        // Box<Number> b = a;  // 1
+        // Container<Number> b = a;  // 1
 
         Container<Float> f = new Container<Float>(3.14f);
         // b.setData(f);        // 2
